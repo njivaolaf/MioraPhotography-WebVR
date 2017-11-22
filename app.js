1b8cf7ced1c69b14e4f3a93826f845ea88f35751
Olaf Ranaivoson <olaf.ranaivoson@agileum.com>
2017-11-22 16:43:47 +0400

    initial setup - part I

--- app.js
+++ app.js
@@ -3,35 +3,52 @@ require('aframe');
 var extendDeep = AFRAME.utils.extendDeep;
 var meshMixins = AFRAME.primitives.getMeshMixin();
 var menuImages;
-function after_load_setup() {
-    console.log('INFO: initial setup');
-    //setup Camera;
-    // your code
-    console.log('document is ready');
+init_SETUP();
 
+function init_SETUP() {
+    set_menu_Images();
+}
+
+
+function after_load_SETUP() {
+    console.log('INFO: initial setup');
     menuImages = document.getElementById('menuImages');
     menuImages.addEventListener('mouseenter', function (event) {
         try {
             switch (event.path[0].id) {
-                default:
-                    console.log('path is =', event.path[0].id);
-                    break;
+                default: console.log('path is =', event.path[0].id);
+                break;
             }
-        }
-        catch (e) {
+        } catch (e) {
             console.log(e);
         }
 
     });
 
-    // someImage = $("landscapeImg");
-    // someImage.mouseenter(function () {
-    //     console.log('Entered');
-    //     someImage.remove();
-    // });
-
 }
 
+function set_menu_Images() {
+    AFRAME.registerPrimitive('a-ocean', {
+        // Attaches the `ocean` component by default.
+        // Defaults the ocean to be parallel to the ground.
+        defaultComponents: {
+            ocean: {},
+            rotation: {
+                x: -90,
+                y: 0,
+                z: 0
+            }
+        },
+        // Maps HTML attributes to the `ocean` component's properties.
+        mappings: {
+            width: 'ocean.width',
+            depth: 'ocean.depth',
+            density: 'ocean.density',
+            color: 'ocean.color',
+            opacity: 'ocean.opacity'
+        }
+    });
+}
 $(document).ready(function () {
-    after_load_setup();
+    after_load_SETUP();
 });
