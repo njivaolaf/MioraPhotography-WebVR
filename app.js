32e923dd492239e4076c316e815d0ccd754e2a95
njivaolaf <njivaolaf@gmail.com>
2017-11-23 00:14:21 +0400

    MFPhotography-WebVR - fadeOut part I

--- app.js
+++ app.js
@@ -3,22 +3,33 @@ require('aframe');
 var extendDeep = AFRAME.utils.extendDeep;
 var meshMixins = AFRAME.primitives.getMeshMixin();
 var menuImages;
+var mainMenu;
+var currentGallery;
+
+// animations
+var fadeOutMenu;
+
 init_SETUP();
 
 function init_SETUP() {
     set_menu_Images();
+    set_animations();
 }
 
 
 function after_load_SETUP() {
     console.log('INFO: initial setup');
-    menuImages = document.getElementById('menuImages');
+    mainMenu = document.querySelector('#mainMenu');
+    currentGallery = document.querySelector('#currentGallery');
+    menuImages = document.querySelector('#menuImages');
     menuImages.addEventListener('mouseenter', function (event) {
         try {
+            // console.log('I was clicked at: ', event.detail.intersection.point);
             switch (event.path[0].id) {
                 default: console.log('path is =', event.path[0].id);
-                break;
+                    break;
             }
+            mainMenu.appendChild(fadeOutMenu);
         } catch (e) {
             console.log(e);
         }
@@ -28,27 +39,25 @@ function after_load_SETUP() {
 }
 
 function set_menu_Images() {
-    AFRAME.registerPrimitive('a-ocean', {
-        // Attaches the `ocean` component by default.
-        // Defaults the ocean to be parallel to the ground.
-        defaultComponents: {
-            ocean: {},
-            rotation: {
-                x: -90,
-                y: 0,
-                z: 0
-            }
-        },
-        // Maps HTML attributes to the `ocean` component's properties.
-        mappings: {
-            width: 'ocean.width',
-            depth: 'ocean.depth',
-            density: 'ocean.density',
-            color: 'ocean.color',
-            opacity: 'ocean.opacity'
-        }
-    });
+    // AFRAME.registerComponent('log', {
+    //     schema: {
+    //         type: 'string'
+    //     },
+    //     init: function () {
+    //         var stringToLog = this.data;
+    //         console.log('stringToLog', stringToLog);
+    //     }
+    // })
 }
+
+
+function set_animations() {
+    fadeOutMenu = document.createElement('a-animation');
+    fadeOutMenu.setAttribute('attribute', 'visible');
+    fadeOutMenu.setAttribute('begin', 1000);
+    fadeOutMenu.setAttribute('to', false);
+};
+
 $(document).ready(function () {
     after_load_SETUP();
 });
